1c66424647305f87151d32c7b2224758613cdbcd
LesnovBrascovitch <104094945+LesnovBrascovitch@users.noreply.github.com>
2023-10-05 14:24:05 +0900

    02

---
+++ Plugins/EnginePlugins/Runtime/Nvidia/Streamline/Source/StreamlineRHI/Public/MyActor.h
@@ -0,0 +1,26 @@
+// Copyright 2021 Starbreeze AB. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MyActor.generated.h"
+
+UCLASS()
+class STREAMLINERHI_API AMyActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMyActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
